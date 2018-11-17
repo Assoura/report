875d11478f54eb950dbbbdd9da2ab792f4797dd7
Théo <39556040+Assoura@users.noreply.github.com>
2018-11-17 14:35:18 +0100

    format index

--- report.py
+++ report.py
@@ -154,7 +154,7 @@ def f_best(prev,spots,path,ville='Paris-France'):
     table = table.sort_values('(km)')
     table = table[(table.iloc[:,:-1].T >= 2).any()]
     del table.index.name
-    table.columns.name = '__Spots (>2*)__'
+    table.columns.name = '_Spots (>2*)_'
     os.chdir(path)
     table.to_html('table.html')
     if ville == 'Paris-France' : suf = ''
